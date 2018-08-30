07c27e72bbd9cbd07b8e456058b587e200ff0d6e
len <1750041@qq.com>
2018-08-30 14:47:26 +0800

    获取绝对路径修改

--- android/src/main/java/com/reactlibrary/utils/FileUtils.java
+++ android/src/main/java/com/reactlibrary/utils/FileUtils.java
@@ -63,13 +63,67 @@ public class FileUtils {
      * @param contentUri
      * @return
      */
-    public static String getFilePathFromURI(Context context, Uri contentUri) {
+    public static String getFilePathFromURI(Activity context, Uri contentUri) {
         //copy file and send new file path
-        String fileName = getFileName(contentUri);
-        if (!TextUtils.isEmpty(fileName)) {
-            File copyFile = new File( Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + fileName);
-            FileUtils.copy(context, contentUri, copyFile);
-            return copyFile.getAbsolutePath();
+//        String fileName = getFileName(contentUri);
+//        if (!TextUtils.isEmpty(fileName)) {
+//            File copyFile = new File( Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + fileName);
+//            FileUtils.copy(context, contentUri, copyFile);
+//            return copyFile.getAbsolutePath();
+//        }
+//        return null;
+
+        return uriToFile(contentUri, context).getAbsolutePath();
+    }
+
+
+    /**
+     * 将uri转换为file
+     * @param uri
+     * @param activity
+     * @return
+     */
+    public static File uriToFile(Uri uri, Activity activity) {
+        String path = null;
+        if ("file".equals(uri.getScheme())) {
+            path = uri.getEncodedPath();
+            if (path != null) {
+                path = Uri.decode(path);
+                ContentResolver cr = activity.getContentResolver();
+                StringBuffer buff = new StringBuffer();
+                buff.append("(").append(MediaStore.Images.ImageColumns.DATA).append("=").append("'" + path + "'").append(")");
+                Cursor cur = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[] { MediaStore.Images.ImageColumns._ID, MediaStore.Images.ImageColumns.DATA }, buff.toString(), null, null);
+                int index = 0;
+                int dataIdx = 0;
+                for (cur.moveToFirst(); !cur.isAfterLast(); cur.moveToNext()) {
+                    index = cur.getColumnIndex(MediaStore.Images.ImageColumns._ID);
+                    index = cur.getInt(index);
+                    dataIdx = cur.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    path = cur.getString(dataIdx);
+                }
+                cur.close();
+                if (index == 0) {
+                } else {
+                    Uri u = Uri.parse("content://media/external/images/media/" + index);
+                    System.out.println("temp uri is :" + u);
+                }
+            }
+            if (path != null) {
+                return new File(path);
+            }
+        } else if ("content".equals(uri.getScheme())) {
+            // 4.2.2以后
+            String[] proj = { MediaStore.Images.Media.DATA };
+            Cursor cursor = activity.getContentResolver().query(uri, proj, null, null, null);
+            if (cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                path = cursor.getString(columnIndex);
+            }
+            cursor.close();
+
+            return new File(path);
+        } else {
+            return null;
         }
         return null;
     }
